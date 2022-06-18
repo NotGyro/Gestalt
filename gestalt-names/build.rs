cd805c85df81fc987ddca4ed66ce43481927f735
Millie "Gyro" C <gyrocoder@gmail.com>
2022-06-18 03:03:21 -0400

    Fix stringcache codegen

--- gestalt-names/build.rs
+++ gestalt-names/build.rs
@@ -3,8 +3,14 @@ extern crate string_cache_codegen;
 use std::path::Path;
 
 fn main() {
+    let generated_path = Path::new("src/generated");
+    if !generated_path.exists() { 
+        std::fs::create_dir_all(generated_path).unwrap();
+    }
+    println!("cargo:rerun-if-changed=build.rs");
+    
     string_cache_codegen::AtomType::new("msgtype::MsgType", "msg_type!")
         .atoms(&["foo", "bar"])
-        .write_to_file(&Path::new("src/generated").join("msg_type.rs"))
+        .write_to_file(&generated_path.join("msg_type.rs"))
         .unwrap()
 }
